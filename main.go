057442b9dbf11fbc6d0dde18b5f9d2657edd5ddb
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-06-15 23:55:38 +0700

    init b-tree data structure

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+)
+
+/*
+| type | nkeys |  pointers  |   offsets  | key-values | unused |
+|  2B  |   2B  | nkeys * 8B | nkeys * 2B |     ...    |        |
+
+| klen | vlen | key | val |
+|  2B  |  2B  | ... | ... |
+*/
+
+const HEADER = 4
+
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+type BNode struct {
+	data []byte // can be dumped to the disk
+}
+
+type BTree struct {
+	// pointer (a nonzero page number)
+	root uint64
+	// callbacks for managing on-disk pages
+	get func(uint64) []byte // dereference a pointer
+	new func([]byte) uint64 // allocate a new page
+	del func(uint64)        // deallocate a page
+}
+
+const (
+	BNODE_NODE = 1 // internal nodes
+	BNODE_LEAF = 2 // leaf nodes
+)
+
+var (
+	ErrBNodeTooLarge   = errors.New("b-tree node exceeds page size")
+	ErrIndexOutOfBound = errors.New("index is out-of-bound")
+)
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	assert(node1max <= BTREE_PAGE_SIZE, ErrBNodeTooLarge)
+}
+
+func assert(assertion bool, err error) {
+	if !assertion {
+		panic(err)
+	}
+}
+
+func (node BNode) byte() uint16 {
+	return binary.LittleEndian.Uint16(node.data[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node.data[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node.data[0:2], btype)
+	binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
+}
+
+// pointers
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys(), ErrIndexOutOfBound)
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node.data[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+	assert(idx < node.nkeys(), ErrIndexOutOfBound)
+	pos := HEADER + 8*idx
+	binary.LittleEndian.PutUint64(node.data[pos:], val)
+}
+
+// offset list
+func offsetPos(node BNode, idx uint16) uint16 {
+	assert(1 <= idx && idx <= node.nkeys(), ErrIndexOutOfBound)
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16) {
+	binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
+}
+
+func (node BNode) kvPos(idx uint16) uint16 {
+	assert(idx < node.nkeys(), ErrIndexOutOfBound)
+	return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	assert(idx < node.nkeys(), ErrIndexOutOfBound)
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node.data[pos:])
+	return node.data[pos+4:][:klen]
+}
+
+func (node BNode) getVal(idx uint16) []byte {
+	assert(idx < node.nkeys(), ErrIndexOutOfBound)
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node.data[pos:])
+	vlen := binary.LittleEndian.Uint16(node.data[pos+2:])
+	return node.data[pos+4+klen:][:vlen]
+}
+
+// node syize in bytes
+func (node BNode) nbytes() uint16 {
+	return node.kvPos(node.nkeys())
+}
+
+func nodeLookupLE(node BNode, key []byte) uint16 {
+	nkeys := node.nkeys()
+	found := uint16(0)
+
+	for i := uint16(1); i < nkeys; i++ {
+		cmp := bytes.Compare(node.getKey(i), key)
+		if cmp <= 0 {
+			found = i
+		}
+		if cmp >= 0 {
+			break
+		}
+	}
+	return found
+}
